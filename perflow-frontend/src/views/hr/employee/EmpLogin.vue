69a300f0476cb96a59fdd8408de210bd6d7d9e3f
Younggi Kim <galaxy138742@gmail.com>
2025-01-06 04:24:46 +0900

    refactor: #162 afterLogin 비동기 처리로 로그인 시간 감소

--- perflow-frontend/src/views/hr/employee/EmpLogin.vue
+++ perflow-frontend/src/views/hr/employee/EmpLogin.vue
@@ -55,7 +55,7 @@ const login = async () => {
 
     authStore.setTokens(accessToken, refreshToken);
 
-    await authStore.afterLogin();
+    authStore.afterLogin();
 
     updateModalVisible(true);
   } catch (error) {
